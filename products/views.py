6e3037ab1fb14341c7f7e3ddb86f0db30f87e435
ivertepny <ivertepny@gmail.com>
2024-08-17 11:44:42 +0300

    added pagination

--- products/views.py
+++ products/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 
 from products.models import Product, ProductCategory, Tag, Basket
 from users.models import User
+from django.core.paginator import Paginator
 
 
 def index(request):
@@ -16,18 +17,24 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request):
+def products(request, category_id=None, page_number=1):
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
+    paginator = Paginator(products, per_page=3)
+    products_paginator = paginator.page(page_number)
+
     context = {
         'title': 'Shop - Каталог',
-        'products': Product.objects.all(),
         'categories': ProductCategory.objects.all(),
+        'products': products_paginator,
         'tags': Tag.objects.all(),
-
     }
     return render(request, 'products/products.html', context)
 
 
-@login_required #декоратор для того, щоб не було корзини у незалогінених користувачів
+@login_required  # декоратор для того, щоб не було корзини у незалогінених користувачів
 def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)
     baskets = Basket.objects.filter(user=request.user, product=product)
