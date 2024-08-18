2545805220ece88776fff107cac8069151f2176b
ivertepny <ivertepny@gmail.com>
2024-08-18 10:29:22 +0300

    Transfer View Product from FBV to CBV

--- products/views.py
+++ products/views.py
@@ -1,37 +1,67 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
 # Create your views here.
 
 from products.models import Product, ProductCategory, Tag, Basket
 from users.models import User
-from django.core.paginator import Paginator
 
 
-def index(request):
-    context = {
-        'title': 'Home',
-        'is_promotion': False,
-    }
-    return render(request, 'products/index.html', context)
+
+class IndexView(TemplateView):
+    template_name = 'products/index.html'
+
+# перевизначаємо метод для додавання Title
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data()
+        context['title'] = 'Store'
+        return context
 
 
-def products(request, category_id=None, page_number=1):
-    if category_id:
-        products = Product.objects.filter(category_id=category_id)
-    else:
-        products = Product.objects.all()
-    paginator = Paginator(products, per_page=3)
-    products_paginator = paginator.page(page_number)
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 3
 
-    context = {
-        'title': 'Shop - Каталог',
-        'categories': ProductCategory.objects.all(),
-        'products': products_paginator,
-        'tags': Tag.objects.all(),
-    }
-    return render(request, 'products/products.html', context)
+    def get_queryset(self):
+        queryset = super(ProductListView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id) if category_id else queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductListView, self).get_context_data()
+        context['title'] = 'Store - Каталог'
+        context['categories'] = ProductCategory.objects.all()
+        # context['tags'] = Tag.objects.all()
+        return context
+
+# переходимо з FBV на CBV
+
+# def index(request):
+#     context = {
+#         'title': 'Home',
+#         'is_promotion': False,
+#     }
+#     return render(request, 'products/index.html', context)
+
+
+# def products(request, category_id=None, page_number=1):
+#     if category_id:
+#         products = Product.objects.filter(category_id=category_id)
+#     else:
+#         products = Product.objects.all()
+#     paginator = Paginator(products, per_page=3)
+#     products_paginator = paginator.page(page_number)
+#
+#     context = {
+#         'title': 'Shop - Каталог',
+#         'categories': ProductCategory.objects.all(),
+#         'products': products_paginator,
+#         'tags': Tag.objects.all(),
+#     }
+#     return render(request, 'products/products.html', context)
 
 
 @login_required  # декоратор для того, щоб не було корзини у незалогінених користувачів
