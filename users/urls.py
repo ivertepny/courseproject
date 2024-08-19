6e12655525310c8bfc25ef362f6966ed60556af7
ivertepny <ivertepny@gmail.com>
2024-08-19 12:20:43 +0300

    Transfer View Login, Logout from FBV to CBV

--- users/urls.py
+++ users/urls.py
@@ -2,16 +2,16 @@
 
 from django.urls import path
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
 
-from users.views import login, UserRegistrationView, UserProfileView, logout
+from users.views import UserLoginView, UserRegistrationView, UserProfileView
 
-app_name = 'users' # Не зрозумів для чого
+app_name = 'users'  # Не зрозумів для чого
 
 urlpatterns = [
-    path('login/', login, name='login'),
+    path('login/', UserLoginView.as_view(), name='login'),
     path('registration/', UserRegistrationView.as_view(), name='registration'),
     path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'),
-    path('logout/', logout, name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
 ]
-
--- users/views.py
+++ users/views.py
@@ -1,29 +1,16 @@
-from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth, messages
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.views.generic.edit import CreateView, UpdateView
 
 from products.models import Basket
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
-from django.contrib.auth.decorators import login_required
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
 # Create your views here.
-def login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            if user:
-                auth.login(request, user)
-                return HttpResponseRedirect(reverse('index'))
-    else:
-        form = UserLoginForm()
-    context = {'form': form}
-    return render(request, 'users/login.html', context)
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
 
 
 class UserRegistrationView(CreateView):
@@ -51,43 +38,3 @@ class UserProfileView(UpdateView):
         context['title'] = 'Store - Кабінет'
         context['baskets'] = Basket.objects.filter(user=self.object)
         return context
-
-
-# def registration(request):
-#     if request.method == 'POST':
-#         form = UserRegistrationForm(data=request.POST)
-#         print(form.is_valid())
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, 'Вітаємо! Ви успішно зареєеструвались')
-#             # messages.success(request, 'Account was created for ' + form.cleaned_data['username'])
-#             return HttpResponseRedirect(reverse('users:login'))
-#     else:
-#         form = UserRegistrationForm()
-#     context = {'form': form}
-#     return render(request, 'users/registration.html', context)
-#
-
-# @login_required
-# def profile(request):
-#     if request.method == 'POST':
-#         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse('users:profile'))
-#         else:
-#             print(form.errors)
-#     else:
-#         form = UserProfileForm(instance=request.user)
-#
-#     context = {
-#         'title': 'Профіль',
-#         'form': form,
-#         'baskets': Basket.objects.filter(user=request.user),
-#     }
-#     return render(request, 'users/profile.html', context)
-
-
-def logout(request):
-    auth.logout(request)
-    return HttpResponseRedirect(reverse('index'))
