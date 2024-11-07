6a98f3f4d689e5e0020dcb3b7030eef61dc7cea6
ivertepny <ivertepny@gmail.com>
2024-11-07 19:35:29 +0200

    try to solve error with STATIC_ROOT

--- postcards_shop/settings.py
+++ postcards_shop/settings.py
@@ -171,8 +171,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-STATIC_ROOT = BASE_DIR / 'static'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Додаємо медіафайли
