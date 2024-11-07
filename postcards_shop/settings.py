f05ddd39b8b2dcf2eeb4bb9a1fe3d79a8d4c2d6a
ivertepny <ivertepny@gmail.com>
2024-11-07 13:47:34 +0200

    update settings alowed hosts

--- postcards_shop/settings.py
+++ postcards_shop/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG')
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ivertepny.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
