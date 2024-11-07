659a8e3351834f9224dd4568ad0958e5898220b1
ivertepny <ivertepny@gmail.com>
2024-11-07 22:16:15 +0200

    try to solve error with AWS 9

--- postcards_shop/settings.py
+++ postcards_shop/settings.py
@@ -321,8 +321,8 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_LOCATION = 'static'
 AWS_S3_REGION_NAME = 'us-east-1'
 
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
 
 # DEFAULT_FILE_STORAGE = 'hillelDjango4.storages.MediaStorage'
