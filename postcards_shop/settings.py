3facd0405974ff452130646c9e16eaf5c1ffe8ce
ivertepny <ivertepny@gmail.com>
2024-11-10 22:59:51 +0200

    testing tests 9

--- postcards_shop/settings.py
+++ postcards_shop/settings.py
@@ -120,9 +120,7 @@ DATABASES = {
         "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT"),
-        'TEST': {
-            'NAME': 'test',  # A separate database name for testing
-        },
+
     }
 }
 
